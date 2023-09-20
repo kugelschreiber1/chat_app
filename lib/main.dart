0ca13aaa1d7a34c8c44af32f9cde49c9c5daa566
Trevor Toni <badrobot.f4@gmail.com>
2023-09-20 22:01:32 +0300

    Added firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,24 @@
 import 'package:chat_app/screens/auth_screen.dart';
+import 'package:chat_app/screens/chat_screen.dart';
+import 'package:chat_app/screens/splash_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const App());
+import 'firebase_options.dart';
+
+final colorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 63, 17, 177),
+);
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    const App(),
+  );
 }
 
 class App extends StatelessWidget {
@@ -14,11 +30,23 @@ class App extends StatelessWidget {
       title: "Chat App",
       theme: ThemeData().copyWith(
         useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color.fromARGB(255, 63, 17, 177),
-        ),
+        colorScheme: colorScheme,
+        scaffoldBackgroundColor: colorScheme.primaryContainer,
       ),
-      home: const AuthScreen(),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const SplashScreen();
+          }
+
+          if (snapshot.hasData) {
+            return const ChatScreen();
+          }
+
+          return const AuthScreen();
+        },
+      ),
     );
   }
 }
