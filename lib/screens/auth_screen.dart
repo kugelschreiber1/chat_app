c7519ef840b9a8a5d1531d27a70767cd5e79507b
Trevor Toni <badrobot.f4@gmail.com>
2023-09-20 22:03:20 +0300

    Added authentication screen

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,7 +1,11 @@
 import 'dart:io';
 
+import 'package:chat_app/screens/login_screen.dart';
 import 'package:chat_app/widgets/image_input.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+final _firebase = FirebaseAuth.instance;
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
@@ -11,15 +15,32 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
-  final _formKey = GlobalKey<FormState>();
+  final _signupFormKey = GlobalKey<FormState>();
   final _emailController = TextEditingController();
   final _usernameController = TextEditingController();
   final _passwordController = TextEditingController();
   final _emailFocusNode = FocusNode();
   final _usernameFocusNode = FocusNode();
   final _passwordFocusNode = FocusNode();
+  String _emailText = '';
+  String _usernameText = '';
+  String _passwordText = '';
+  bool _hidePassword = true;
   File? _selectedImage;
   String? _error;
+  bool _isLogin = false;
+
+  void _showSignupMessage(String? errorMessage) {
+    if (!context.mounted) {
+      return;
+    }
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(errorMessage ?? 'Authentication failed'),
+      ),
+    );
+  }
 
   void _onAddImage() {
     showModalBottomSheet(
@@ -34,10 +55,36 @@ class _AuthScreenState extends State<AuthScreen> {
     );
   }
 
-  void _onSignup() {
-    if (_formKey.currentState!.validate()) {
-      _formKey.currentState!.save();
-      _formKey.currentState!.reset();
+  void _onSignup() async {
+    final isValid = _signupFormKey.currentState!.validate();
+
+    if (isValid) {
+      _signupFormKey.currentState!.save();
+      /*This method will send a HTTP request to firebase*/
+      try {
+        await _firebase.createUserWithEmailAndPassword(
+          email: _emailText,
+          password: _passwordText,
+        );
+      } on FirebaseAuthException catch (error) {
+        if (error.code == 'email-already-in-use') {
+          _showSignupMessage(error.message!);
+        }
+
+        if (error.code == 'invalid-email') {
+          _showSignupMessage(error.message!);
+        }
+
+        if (error.code == 'operation-not-allowed') {
+          _showSignupMessage(error.message!);
+        }
+
+        if (error.code == 'weak-password') {
+          _showSignupMessage(error.message!);
+        }
+
+        _signupFormKey.currentState!.reset();
+      }
     }
   }
 
@@ -60,7 +107,214 @@ class _AuthScreenState extends State<AuthScreen> {
       message = "Change Image";
     }
 
+    Widget screenContent = Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          height: 100.0,
+          width: 100.0,
+          alignment: Alignment.center,
+          child: Image.asset(
+            "assets/images/chat.png",
+            fit: BoxFit.cover,
+          ),
+        ),
+        const SizedBox(height: 30.0),
+        Container(
+          decoration: BoxDecoration(
+            border: Border.all(),
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          padding: const EdgeInsets.symmetric(
+            horizontal: 12.0,
+            vertical: 16.0,
+          ),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: profilePicture,
+                ),
+                const SizedBox(height: 16.0),
+                ElevatedButton.icon(
+                  onPressed: _onAddImage,
+                  icon: const Icon(Icons.camera),
+                  label: Text(message),
+                ),
+                Form(
+                  key: _signupFormKey,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      TextFormField(
+                        autocorrect: false,
+                        textCapitalization: TextCapitalization.none,
+                        maxLength: 100,
+                        focusNode: _emailFocusNode,
+                        controller: _emailController,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: const InputDecoration(
+                          label: Text("Email"),
+                          contentPadding: EdgeInsets.all(8.0),
+                        ),
+                        validator: (value) {
+                          String email;
+
+                          if (value == null || value.isEmpty) {
+                            _error = "Email cannot be empty";
+                            return _error;
+                          }
+
+                          email = value.trim();
+
+                          if (email.length > 100) {
+                            _error = "Email is too long";
+                            return _error;
+                          }
+                          if (!RegExp(
+                                  r'^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$')
+                              .hasMatch(email)) {
+                            _error = 'Enter a valid email address';
+                            return _error;
+                          }
+                          return null;
+                        },
+                        onFieldSubmitted: (value) {
+                          FocusScope.of(context)
+                              .requestFocus(_usernameFocusNode);
+                        },
+                        onSaved: (value) {
+                          _emailText = value!;
+                        },
+                      ),
+                      const SizedBox(height: 10.0),
+                      TextFormField(
+                        autocorrect: false,
+                        maxLength: 10,
+                        focusNode: _usernameFocusNode,
+                        controller: _usernameController,
+                        keyboardType: TextInputType.name,
+                        decoration: const InputDecoration(
+                          label: Text("Username"),
+                          contentPadding: EdgeInsets.all(8.0),
+                        ),
+                        validator: (value) {
+                          String username;
+
+                          if (value == null || value.isEmpty) {
+                            _error = "Username cannot be empty";
+                            return _error;
+                          }
+                          username = value.trim();
+
+                          if (username.length < 3) {
+                            _error = "Username is too short";
+                            return _error;
+                          }
+                          if (username.length > 10) {
+                            _error = "Username is too long";
+                            return _error;
+                          }
+                          return null;
+                        },
+                        onFieldSubmitted: (value) {
+                          FocusScope.of(context)
+                              .requestFocus(_passwordFocusNode);
+                        },
+                        onSaved: (value) {
+                          _usernameText = value!;
+                        },
+                      ),
+                      const SizedBox(height: 10.0),
+                      Row(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(
+                            child: TextFormField(
+                              focusNode: _passwordFocusNode,
+                              controller: _passwordController,
+                              decoration: const InputDecoration(
+                                label: Text("Password"),
+                                contentPadding: EdgeInsets.all(8.0),
+                              ),
+                              obscureText: _hidePassword,
+                              validator: (value) {
+                                String password;
+
+                                if (value == null || value.isEmpty) {
+                                  _error = "Password cannot be empty";
+                                  return _error;
+                                }
+
+                                password = value.trim();
+
+                                if (password.length < 8) {
+                                  _error =
+                                      "Password should have 8 or more characters";
+                                  return _error;
+                                }
+                                if (password.length > 100) {
+                                  _error =
+                                      "Password should have 100 or less characters";
+                                  return _error;
+                                }
+                                return null;
+                              },
+                              onFieldSubmitted: (value) {
+                                if (_passwordFocusNode.hasFocus) {
+                                  FocusScope.of(context).unfocus();
+                                }
+                              },
+                              onSaved: (value) {
+                                _passwordText = value!;
+                              },
+                            ),
+                          ),
+                          TextButton(
+                              onPressed: () {
+                                setState(() {
+                                  _hidePassword = !_hidePassword;
+                                });
+                              },
+                              child: _hidePassword == true
+                                  ? const Text("Show")
+                                  : const Text("Hide")),
+                        ],
+                      ),
+                      const SizedBox(height: 20.0),
+                      ElevatedButton(
+                        onPressed: _onSignup,
+                        child: const Text("Signup"),
+                      ),
+                      const SizedBox(height: 20.0),
+                      TextButton(
+                        onPressed: () {
+                          setState(() {
+                            _isLogin = !_isLogin;
+                          });
+                        },
+                        child: const Text("I already have an account"),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+
+    if (_isLogin) {
+      screenContent = const LoginScreen();
+    }
+
     return Scaffold(
+      backgroundColor: Theme.of(context).copyWith().scaffoldBackgroundColor,
       body: SafeArea(
         child: Container(
           padding: const EdgeInsets.symmetric(
@@ -69,155 +323,7 @@ class _AuthScreenState extends State<AuthScreen> {
           ),
           alignment: Alignment.center,
           child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Container(
-                  height: 100.0,
-                  width: 100.0,
-                  alignment: Alignment.center,
-                  child: Image.asset(
-                    "assets/images/chat.png",
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                const SizedBox(height: 30.0),
-                Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(),
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 12.0,
-                    vertical: 16.0,
-                  ),
-                  child: SingleChildScrollView(
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        CircleAvatar(
-                          radius: 50.0,
-                          backgroundImage: profilePicture,
-                        ),
-                        const SizedBox(height: 16.0),
-                        ElevatedButton.icon(
-                          onPressed: _onAddImage,
-                          icon: const Icon(Icons.camera),
-                          label: Text(message),
-                        ),
-                        Form(
-                          key: _formKey,
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              TextFormField(
-                                maxLength: 100,
-                                focusNode: _emailFocusNode,
-                                controller: _emailController,
-                                keyboardType: TextInputType.emailAddress,
-                                decoration: const InputDecoration(
-                                  label: Text("Email"),
-                                  contentPadding: EdgeInsets.all(8.0),
-                                ),
-                                validator: (value) {
-                                  if (value == null || value.isEmpty) {
-                                    _error = "Email cannot be empty";
-                                    return _error;
-                                  }
-                                  if (value.length > 100) {
-                                    _error = "Email is too long";
-                                    return _error;
-                                  }
-                                  if (!RegExp(
-                                          r'^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$')
-                                      .hasMatch(value)) {
-                                    _error = 'Enter a valid email address';
-                                    return _error;
-                                  }
-                                  return null;
-                                },
-                                onFieldSubmitted: (value) {
-                                  FocusScope.of(context)
-                                      .requestFocus(_usernameFocusNode);
-                                },
-                              ),
-                              TextFormField(
-                                maxLength: 10,
-                                focusNode: _usernameFocusNode,
-                                controller: _usernameController,
-                                keyboardType: TextInputType.name,
-                                decoration: const InputDecoration(
-                                  label: Text("Username"),
-                                  contentPadding: EdgeInsets.all(8.0),
-                                ),
-                                validator: (value) {
-                                  if (value == null || value.isEmpty) {
-                                    _error = "Username cannot be empty";
-                                    return _error;
-                                  }
-                                  if (value.length < 3) {
-                                    _error = "Username is too short";
-                                    return _error;
-                                  }
-                                  if (value.length > 10) {
-                                    _error = "Username is too long";
-                                    return _error;
-                                  }
-                                  return null;
-                                },
-                                onFieldSubmitted: (value) {
-                                  FocusScope.of(context)
-                                      .requestFocus(_passwordFocusNode);
-                                },
-                              ),
-                              TextFormField(
-                                focusNode: _passwordFocusNode,
-                                controller: _passwordController,
-                                keyboardType: TextInputType.visiblePassword,
-                                decoration: const InputDecoration(
-                                  label: Text("Password"),
-                                  contentPadding: EdgeInsets.all(8.0),
-                                ),
-                                validator: (value) {
-                                  if (value == null || value.isEmpty) {
-                                    _error = "Password cannot be empty";
-                                    return _error;
-                                  }
-                                  if (value.length < 8) {
-                                    _error = "Password is too short";
-                                    return _error;
-                                  }
-                                  if (value.length > 100) {
-                                    _error = "Password is too long";
-                                    return _error;
-                                  }
-                                  return null;
-                                },
-                                onFieldSubmitted: (value) {
-                                  FocusScope.of(context)
-                                      .requestFocus(_passwordFocusNode);
-                                },
-                              ),
-                              const SizedBox(height: 20.0),
-                              ElevatedButton(
-                                onPressed: _onSignup,
-                                child: const Text("Signup"),
-                              ),
-                              const SizedBox(height: 20.0),
-                              TextButton(
-                                onPressed: () {},
-                                child: const Text("I already have an account"),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
+            child: screenContent,
           ),
         ),
       ),
